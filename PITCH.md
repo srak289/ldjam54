234e7570eeacbff773584aa5131d3e6991c27b16
srak <spencer.rak@gmail.com>
2023-09-30 09:54:43 -0400

    Snapshot

---
+++ PITCH.md
@@ -0,0 +1,37 @@
+# Ideas
+
+##PVE
+
+### Options
+- mazebuilder
+- tcg
+- top-down
+
+
+### Goal
+- score grinder
+- survival
+
+
+### Fun
+- 
+
+
+
+map can go to zero
+fragile tiles elected one (or more for higher difficulty) per row every cycle
+
+pickups can be buffs
+ - adrenaline
+ - immune to plague
+ - perhaps a switch to disable the compactor
+ - perhaps a switch to 
+
+
+ tiles can have fragile and also pickup attributes
+
+ loss condition is either map goes to zero or you are on a fragile tile during compression cycle
+
+ enemies move towards player
+
+ health mechanic
---
+++ waste_management/base.py
@@ -0,0 +1,43 @@
+from pygame import *
+import pygame
+
+
+class TileCharacteristic(enum.Enum):
+    KEY = 0
+    EXIT = 1
+    FOOD = 2
+    GLASS = 3
+    DEATH = 4
+    PLAGUE = 5
+    SLICK = 6
+    NORMAL = 10
+
+
+class Tile(pygame.sprite.Sprite:
+    pass
+
+
+class Grid:
+
+    def __init__(self):
+        self._tiles = []
+
+    def get_tile_at(self, x, y):
+        try:
+            return self._tiles[x][y]
+        except IndexArrayError:
+            raise
+
+    def blit(self, canvas):
+        for row in self._tiles:
+            for tile in row:
+                canvas.blit(tile.surface, tile.rect)
+            
+        
+
+
+class Player:
+    pass
+
+class Enemy:
+    pass
