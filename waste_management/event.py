c2440e949f6bf9cba457a0c46ec429447f9d672a
srak <spencer.rak@gmail.com>
2023-10-09 07:38:06 -0400

    Rename events

--- waste_management/event.py
+++ waste_management/event.py
@@ -7,6 +7,8 @@ i = USEREVENT
 for n in (
     "GAME_STOP",
     "GAME_RUN",
+    "GAME_WIN",
+    "GAME_LOSE",
 
     "MENU_STOP",
     "MENU_RUN",
@@ -18,18 +20,18 @@ for n in (
     "GRID_COLLAPSE",
 
 
-    "PLAYER_UP",
-    "PLAYER_DOWN",
-    "PLAYER_LEFT",
-    "PLAYER_RIGHT",
-    "PLAYER_INVENTORY",
+    "PLAYER_CONTROL_UP",
+    "PLAYER_CONTROL_DOWN",
+    "PLAYER_CONTROL_LEFT",
+    "PLAYER_CONTROL_RIGHT",
+    "PLAYER_CONTROL_INVENTORY",
 
-    "PLAYER_CRUSHED",
-    "PLAYER_KILLED",
-    "PLAYER_INJURED",
-    "PLAYER_INFECTED",
-    "PLAYER_SLIP",
-    "PLAYER_PICKUP",
+    "PLAYER_EFFECT_CRUSHED",
+    "PLAYER_EFFECT_KILLED",
+    "PLAYER_EFFECT_INJURED",
+    "PLAYER_EFFECT_INFECTED",
+    "PLAYER_EFFECT_SLIP",
+    "PLAYER_EFFECT_PICKUP",
 ):
     i += 1
     locals()[n] = Event(i, message=n)
